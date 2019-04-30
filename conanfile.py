d614008ee132eaefe8f3d70df4e05e668d31495a
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2019-04-30 10:30:48 +0200

    adapt code to work with COLLIER 1.2.3

--- conanfile.py
+++ conanfile.py
@@ -1,29 +1,34 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanException
 
 
 class CollierConan(ConanFile):
     name = "COLLIER"
     version = "1.2.3"
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Collier here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
+    license = "GPL-3.0-only"
+    author = "Alexander Voigt"
+    url = "https://collier.hepforge.org/"
+    description = "A Complex One-Loop Library with Extended Regularizations"
+    topics = ("HEP")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
-    generators = "cmake"
-    exports_sources = "src/*"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    exports = ["LICENSE", "FindCOLLIER.cmake"]
+    default_options = "shared=False", "fPIC=True"
+    generators = ["cmake", "make", "pkg_config"]
+    _source_subfolder = "COLLIER-{}".format(version)
+
+    def source(self):
+        src_file = "https://collier.hepforge.org/downloads/collier-{}.tar.gz".format(self.version)
+
+        try:
+            tools.get(src_file)
+        except ConanException:
+            raise ConanException("Could not download source code {}".format(src_file))
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="src")
+        cmake.configure(source_folder=self._source_subfolder)
         cmake.build()
-
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
         self.copy("*.h", dst="include", src="src")
@@ -32,6 +37,8 @@ class CollierConan(ConanFile):
         self.copy("*.dylib*", dst="lib", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("COPYING", src=self._source_subfolder, dst="licenses", keep_path=False)
+        self.copy('FindCOLLIER.cmake', '.', '.')
 
     def package_info(self):
-        self.cpp_info.libs = ["hello"]
+        self.cpp_info.libs = ["collier"]
