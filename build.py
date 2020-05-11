b919786a62dc97241768d70df6ba6b1fe7ff32a2
Alexander Voigt <ahgvoigt@gmail.com>
2020-05-11 22:44:20 +0200

    disable cross-compilation

--- build.py
+++ build.py
@@ -3,5 +3,5 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds()
+    builder.add(settings={"arch": "x86_64"})
     builder.run()
